a8f0dd281bc000d09a328b726ce05165fc94ab73
Nayana <Nayana@appinsight.tech>
2022-09-15 18:12:25 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/abhinavdelivery/store/abhinav_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abhinavdelivery.store.abhinav_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/appinsight/grocbaydelivery/grocbay_delivery_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abhinavdelivery.store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
